4a49ba1bd706c466ec0235536f1a38046b3a0ae6
samuelreichor <samuelreichor@gmail.com>
2025-07-24 16:52:08 +0200

    I am Batman

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for llmify
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace samuelreichor\llmify\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * llmify settings
+ */
+class Settings extends Model
+{
+}
